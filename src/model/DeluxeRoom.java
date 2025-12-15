5b191b6a500cfd6c3e44d4280a35bb77c1fd96ac
Fatih Çatalçam <fthctlcm@outlook.com>
2025-12-15 14:42:19 +0300

    Add DeluxeRoom and StandardRoom subclasses with price calculation
    Implemented inheritance and polymorphism by overriding calculatePrice() in DeluxeRoom and StandardRoom.

--- src/model/DeluxeRoom.java
+++ src/model/DeluxeRoom.java
@@ -1,5 +1,19 @@
 package model;
 
-public class DeluxeRoom {
+public class DeluxeRoom extends Room {
 
+    public DeluxeRoom(int roomNumber, double basePrice, int capacity) {
+        super(roomNumber, basePrice, capacity);
+    }
+
+    @Override
+    public double calculatePrice() {
+        // Deluxe odalarda %30 ekstra ücret
+        return basePrice * 1.30;
+    }
+
+    @Override
+    public String toString() {
+        return "Deluxe Room #" + roomNumber + " | Capacity: " + capacity + " | Price: " + calculatePrice();
+    }
 }
--- src/model/StandartRoom.java
+++ src/model/StandartRoom.java
@@ -1,5 +1,19 @@
 package model;
 
-public class StandartRoom {
+public class StandardRoom extends Room {
 
+    public StandardRoom(int roomNumber, double basePrice, int capacity) {
+        super(roomNumber, basePrice, capacity);
+    }
+
+    @Override
+    public double calculatePrice() {
+        // Standart odalar direkt basePrice
+        return basePrice;
+    }
+
+    @Override
+    public String toString() {
+        return "Standard Room #" + roomNumber + " | Capacity: " + capacity + " | Price: " + calculatePrice();
+    }
 }
